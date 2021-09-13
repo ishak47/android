0afc52300fd377a086f829f263047e81ce382be9
ishak <ijmilou@roc-dev.com>
2021-09-13 12:48:48 +0200

    ordening files

---
+++ userInput/callingMethods/app/src/main/java/com/example/android/callingmethods/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.android.callingmethods;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        View textView = findViewById(R.id.title);
+        textView.setVisibility(View.GONE);
+    }
+}

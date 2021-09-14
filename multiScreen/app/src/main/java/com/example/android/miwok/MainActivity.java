20a88da228b42755c467daa2419358ab0e0d5da8
ishak <ijmilou@roc-dev.com>
2021-09-14 08:37:30 +0200

    test

--- multiScreen/app/src/main/java/com/example/android/miwok/MainActivity.java
+++ multiScreen/app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.miwok;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
 
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
+    }
+
+    public void openNumbersList(View View){
+
     }
 }

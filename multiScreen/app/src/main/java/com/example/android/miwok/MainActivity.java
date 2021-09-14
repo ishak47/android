97b3c165d4c8cc23d0999b7e6a1be7f98a7abc73
ishak <ijmilou@roc-dev.com>
2021-09-14 09:58:00 +0200

    testen

--- multiScreen/app/src/main/java/com/example/android/miwok/MainActivity.java
+++ multiScreen/app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -3,17 +3,13 @@ package com.example.android.miwok;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-<<<<<<< Updated upstream
 import android.view.View;
-=======
->>>>>>> Stashed changes
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-<<<<<<< Updated upstream
 
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
@@ -22,8 +18,4 @@ public class MainActivity extends AppCompatActivity {
     public void openNumbersList(View View){
 
     }
-=======
-        setContentView(R.layout.activity_main);
-    }
->>>>>>> Stashed changes
 }
